b6a1b0f2da6e81de10d7271faed62560ecd45e8d
“Kawsahr <kawshar798@gmail.com>
2020-06-20 23:58:21 +0600

    setup project and complete header

---
+++ src/pages/Room.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '../components/Hero';
+
+const Room = () => {
+    return (
+        <div>
+           <Hero hero="roomsHero"/>
+        </div>
+    );
+};
+
+export default Room;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleRoom = () => {
+    return (
+        <div>
+            <h2>Single page</h2>
+        </div>
+    );
+};
+
+export default SingleRoom;
